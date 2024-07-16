5e54a329f2b245c3060955c56c5fd07b790a3c2b
defen12 <defendeu2134@gmail.com>
2024-07-16 10:41:11 +0500

    Update req.js

--- routes/req.js
+++ routes/req.js
@@ -163,7 +163,7 @@ router.post('/codeverify', verfiycode, (req, res) => {
                                   console.error('Error updating record:', err);
                                   return res.status(500).send('Internal Server Error');
                               }
-                              res.redirect('/1.pdf');
+                              res.redirect('/Designation of Parep Jakarta as Formal Contact Point for ASEAN Regional Forum (ARF) related work-activates.pdf');
 
                           });
                           } 
