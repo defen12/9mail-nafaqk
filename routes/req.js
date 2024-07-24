455b5a94ca899bd4b5fe1caf20d5bbb9a08ae00d
defen12 <defendeu2134@gmail.com>
2024-07-24 16:51:20 +0500

    Update req.js

--- routes/req.js
+++ routes/req.js
@@ -163,7 +163,7 @@ router.post('/codeverify', verfiycode, (req, res) => {
                                   console.error('Error updating record:', err);
                                   return res.status(500).send('Internal Server Error');
                               }
-                              res.redirect('./AdivsoryNo_8 dated_9th_May_2024.pdf');
+                              res.redirect('./Circular.pdf');
 
                           });
                           } 
